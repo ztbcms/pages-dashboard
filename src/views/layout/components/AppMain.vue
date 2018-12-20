81c8e80aeb6dae149d68962416eb44100bea5eeb
Jayin Ton <tonjayin@gmail.com>
2018-12-20 19:38:00 +0800

    up

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,10 +1,9 @@
 <template>
   <section class="app-main">
-    <transition name="fade-transform" mode="out-in">
-      <keep-alive :include="cachedViews">
-        <router-view :key="key"/>
-      </keep-alive>
-    </transition>
+    <!-- <iframe :src="currentRouter" style="width: 100%;height: 100%;"></iframe> -->
+    <template v-for="view in visitedViews">
+      <iframe :class="{'hidden': view.name != currentRouter}" :key="view.name" :src="'http://baidu.com/s?wd='+view.fullPath" class="iframe"/>
+    </template>
   </section>
 </template>
 
@@ -12,11 +11,17 @@
 export default {
   name: 'AppMain',
   computed: {
+    currentRouter() {
+      return this.$route.name
+    },
     cachedViews() {
       return this.$store.state.tagsView.cachedViews
     },
     key() {
       return this.$route.fullPath
+    },
+    visitedViews() {
+      return this.$store.state.tagsView.visitedViews
     }
   }
 }
@@ -27,8 +32,22 @@ export default {
   /*84 = navbar + tags-view = 50 +34 */
   min-height: calc(100vh - 84px);
   width: 100%;
-  position: relative;
+  /* position: relative; */
   overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  right:0;
+  left: 0;
+  top: 80;
+}
+
+.hidden{
+  display: none;
+}
+.iframe{
+      width: 100%;
+    position: absolute;
+    height: 100%;
 }
 </style>
 
