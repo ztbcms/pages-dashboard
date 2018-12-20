9ff62ad3cd899c5ae3f3290856cbeb4e0a4ac5fe
Jayin Ton <tonjayin@gmail.com>
2018-12-20 20:21:24 +0800

    up;

---
+++ src/views/Launch/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+    empty
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Launch',
+  components: {
+
+  },
+  mixins: [],
+  computed: {
+
+  },
+  methods: {
+    getMenuList() {
+
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -13,6 +13,7 @@
 <script>
 import { Navbar, Sidebar, AppMain, TagsView } from './components'
 import ResizeMixin from './mixin/ResizeHandler'
+import Launch from '@/views/Launch'
 
 export default {
   name: 'Layout',
@@ -39,33 +40,67 @@ export default {
       }
     }
   },
+  mounted() {
+    console.log('layout mounted')
+    this.getMenuList()
+  },
   methods: {
     handleClickOutside() {
       this.$store.dispatch('closeSideBar', { withoutAnimation: false })
+    },
+    getMenuList() {
+      const menuList = [
+        {
+          path: 'Admin/Config/index',
+          component: Launch,
+          name: 'Admin/Config/index',
+          // redirect: "/documentation/index",
+          meta: {
+            title: '我的面板',
+            icon: 'documentation',
+            noCache: true
+          },
+          children: [
+            {
+              path: 'Admin/Adminmanage/myinfo',
+              component: Launch,
+              name: '修改个人信息',
+              meta: {
+                title: '修改个人信息',
+                icon: 'documentation',
+                noCache: true
+              }
+            }
+          ]
+        }
+      ]
+
+      console.log('menuList', menuList)
+      this.$router.addRoutes(menuList)
     }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import "~@/styles/mixin.scss";
-  .app-wrapper {
-    @include clearfix;
-    position: relative;
-    height: 100%;
-    width: 100%;
-    &.mobile.openSidebar{
-      position: fixed;
-      top: 0;
-    }
-  }
-  .drawer-bg {
-    background: #000;
-    opacity: 0.3;
-    width: 100%;
+@import "~@/styles/mixin.scss";
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  &.mobile.openSidebar {
+    position: fixed;
     top: 0;
-    height: 100%;
-    position: absolute;
-    z-index: 999;
   }
+}
+.drawer-bg {
+  background: #000;
+  opacity: 0.3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
+}
 </style>
