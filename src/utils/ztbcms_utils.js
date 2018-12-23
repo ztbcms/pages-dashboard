bb04e21dcbffa5cd42f446cc8bee4b7ccea9cb48
Jayin Ton <tonjayin@gmail.com>
2018-12-24 02:46:32 +0800

    第1层级菜单才添加上默认的icon

--- src/utils/ztbcms_utils.js
+++ src/utils/ztbcms_utils.js
@@ -3,9 +3,10 @@ import Layout from '@/views/layout/Layout'
 /**
  * 解析菜单
  * @param menuList
+ * @param level 层级
  * @returns {Array}
  */
-export function parserMenuList(menuList) {
+export function parserMenuList(menuList, level = 1) {
   const result = []
   if (menuList) {
     menuList.forEach((menu) => {
@@ -17,14 +18,15 @@ export function parserMenuList(menuList) {
         redirect: '',
         meta: {
           title: menu.name,
-          icon: menu.icon,
-          url: menu.url
+          icon: menu.icon || (level === 1 ? 'dashboard' : ''), // 第1层级才添加上默认的icon
+          url: menu.url,
+          level: level
         },
         children: []
       }
 
       if (menu.items && menu.items.length > 0) {
-        item.children = parserMenuList(menu.items)
+        item.children = parserMenuList(menu.items, level + 1)
         item.redirect = item.children[0].path
       }
       result.push(item)
