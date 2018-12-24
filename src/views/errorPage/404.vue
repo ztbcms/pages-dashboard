620afcbe597692f724201bd1e64c3cf3c6c4ae7a
Jayin Ton <tonjayin@gmail.com>
2018-12-24 15:47:11 +0800

    更换错误页图片

--- src/views/errorPage/404.vue
+++ src/views/errorPage/404.vue
@@ -2,10 +2,8 @@
   <div class="wscn-http404-container">
     <div v-if="show" class="wscn-http404">
       <div class="pic-404">
-        <img class="pic-404__parent" src="@/assets/404_images/404.png" alt="404">
-        <img class="pic-404__child left" src="@/assets/404_images/404_cloud.png" alt="404">
-        <img class="pic-404__child mid" src="@/assets/404_images/404_cloud.png" alt="404">
-        <img class="pic-404__child right" src="@/assets/404_images/404_cloud.png" alt="404">
+        <img class="pic-404__parent" src="@/assets/400_images/400.png" alt="404">
+
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
